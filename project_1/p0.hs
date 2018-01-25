f98b0cf3b27459424e483461124b0a02950cb311
Tim Elvart <telvart@ku.edu>
2018-01-25 11:30:36 -0600

    initialize eecs 662

---
+++ project_1/p0.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE GADTs, FlexibleContexts #-}
+
+-- Imports for QuickCheck
+import System.Random
+import Test.QuickCheck
+import Test.QuickCheck.Gen
+import Test.QuickCheck.Function
+import Test.QuickCheck.Monadic
+
+-- Imports for Parsec
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Language
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Token
+
+--
+-- Simple caculator over naturals with no identifiers
+--
+-- Author: Perry Alexander
+-- Date: Tue Jan 23 17:54:44 CST 2018
+--
+-- Source files for the Arithmetic Expressions (AE) language from PLIH
+--
+
+-- AST Definition
+
+data AE where
+  Num :: Int -> AE
+  Plus :: AE -> AE -> AE
+  Minus :: AE -> AE -> AE
+  Mult :: AE -> AE -> AE
+  Div :: AE -> AE -> AE
+  If0 :: AE -> AE -> AE -> AE
+  deriving (Show,Eq)
+
+-- AE Parser (Requires ParserUtils and Parsec included above)
+
+languageDef =
+  javaStyle { identStart = letter
+            , identLetter = alphaNum
+            , reservedNames = [ "if0"
+                              , "then"
+                              , "else"
+                              ]
+            , reservedOpNames = [ "+","-","*","/"]
+            }
+
+lexer = makeTokenParser languageDef
+
+inFix o c a = (Infix (reservedOp lexer o >> return c) a)
+preFix o c = (Prefix (reservedOp lexer o >> return c))
+postFix o c = (Postfix (reservedOp lexer o >> return c))
+
+parseString p str =
+  case parse p "" str of
+    Left e -> error $ show e
+    Right r -> r
+
+expr :: Parser AE
+expr = buildExpressionParser operators term
+
+operators = [
+  [ inFix "*" Mult AssocLeft
+    , inFix "/" Div AssocLeft ]
+  , [ inFix "+" Plus AssocLeft
+  , inFix "-" Minus AssocLeft ]
+  ]
+
+numExpr :: Parser AE
+numExpr = do i <- integer lexer
+             return (Num (fromInteger i))
+
+ifExpr :: Parser AE
+ifExpr  = do reserved lexer "if0"
+             c <- expr
+             reserved lexer "then"
+             t <- expr
+             reserved lexer "else"
+             e <- expr
+             return (If0 c t e)
+
+
+term = parens lexer expr
+       <|> numExpr
+       <|> ifExpr
+
+-- Parser invocation
+-- Call parseAE to parse a string into the AE data structure.
+
+parseAE = parseString expr
+
+-- Evaluation Functions
+-- Replace the bodies of these functions with your implementations for
+-- Exercises 1-4.  Feel free to add utility functions or testing functions as
+-- you see fit, but do not change the function signatures.  Note that only
+-- Exercise 4 requires you to integrate the parser above.
+
+evalAE :: AE -> AE
+evalAE _ = (Num 0)
+
+evalAEMaybe :: AE -> Maybe AE
+evalAEMaybe _ = Nothing
+
+evalM :: AE -> Maybe AE
+evalM _ = Nothing
+
+interpAE :: String -> Maybe AE
+interpAE _ = Nothing
