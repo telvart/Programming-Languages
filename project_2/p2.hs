f31f475d0273b6d345ad91adb748b9a44c2fef2a
Tim Elvart <telvart@ku.edu>
2018-03-12 11:07:02 -0500

    add p2

---
+++ project_2/p2.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE GADTs,FlexibleContexts #-}
+
+-- Imports for Monads
+
+import Control.Monad
+
+-- BBAE AST and Type Definitions
+
+data TBBAE where
+  TNum :: TBBAE
+  TBool :: TBBAE
+  deriving (Show,Eq)
+
+data BBAE where
+  Num :: Int -> BBAE
+  Plus :: BBAE -> BBAE -> BBAE
+  Minus :: BBAE -> BBAE -> BBAE
+  Bind :: String -> BBAE -> BBAE -> BBAE
+  Id :: String -> BBAE
+  Boolean :: Bool -> BBAE
+  And :: BBAE -> BBAE -> BBAE
+  Leq :: BBAE -> BBAE -> BBAE
+  IsZero :: BBAE -> BBAE
+  If :: BBAE -> BBAE -> BBAE -> BBAE
+  deriving (Show,Eq)
+
+type Env = [(String,BBAE)]
+
+type Cont = [(String,TBBAE)]
+
+evalS :: BBAE -> (Maybe BBAE)
+evalS _ = Nothing
+
+evalM :: Env -> BBAE -> (Maybe BBAE)
+evalM _ _ = Nothing
+
+testBBAE :: BBAE -> Bool
+testBBAE _ = True
+
+typeofM :: Cont -> BBAE -> (Maybe TBBAE)
+typeofM _ _ = Nothing
+
+evalT :: BBAE -> (Maybe BBAE)
+evalT _ = Nothing
+
